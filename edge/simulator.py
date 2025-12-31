eafc149b7c3f31c02be4a438056e1a8a95209a3f
joaodavidesouza <joaodavidesouza@protonmail.com>
2025-12-31 12:35:26 -0300

    fix: changed portuguese comments and variables to english

--- edge/simulator.py
+++ edge/simulator.py
@@ -4,35 +4,35 @@ import time
 import random
 import paho.mqtt.client as mqtt
 
-# Lemos as configurações de variáveis de ambiente (ou usamos um padrão)
+# Environment variables or defaults
 BROKER = os.getenv("MQTT_BROKER", "localhost") 
 PORT = int(os.getenv("MQTT_PORT", 1883))
-TOPIC = os.getenv("MQTT_TOPIC", "industria/textil/maquina1")
+TOPIC = os.getenv("MQTT_TOPIC", "industry/textile/machine1")
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
-# Função para tentar conectar até conseguir
 def connect_to_broker():
     while True:
         try:
             client.connect(BROKER, PORT, 60)
-            print(f"✅ Conectado ao Broker: {BROKER}")
+            print(f"Connected to Broker: {BROKER}")
             break
         except Exception as e:
-            print(f"⏳ Aguardando Broker... ({e})")
+            print(f"Waiting for Broker... ({e})")
             time.sleep(5)
 
 connect_to_broker()
 
 try:
     while True:
-        dados = {
-            "machine_id": "tear-01-mock",
+        data = {
+            "machine_id": "loom-01-mock",
             "vibration": round(random.uniform(0.1, 0.8), 2),
             "timestamp": time.time()
         }
-        client.publish(TOPIC, json.dumps(dados))
-        print(f"📡 Enviado para {BROKER}: {dados}")
+        client.publish(TOPIC, json.dumps(data))
+        print(f"Sent to {BROKER}: {data}")
         time.sleep(2)
 except KeyboardInterrupt:
     client.disconnect()
+    print("Disconnected.")
